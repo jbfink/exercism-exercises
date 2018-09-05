6db98a69fb0715c526f443fc1871339acbc3a02b
John Fink <john.fink@gmail.com>
2018-09-05 19:48:28 -0400

    Refactored for one line with boolean OR, still needs test for nil and return.

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -5,10 +5,8 @@ package twofer
 // one for me." if name == "Alice", "One for Bob, one for me." if name == "Bob", and "One for you, one for me." if name is anything else.
 
 func ShareWith(name string) string {
-	if name == "Alice" {
-		return "One for Alice, one for me."
-	} else if name == "Bob" {
-		return "One for Bob, one for me."
+	if name == "Alice" || name == "Bob" {
+		return "One for " + name + ", one for me."
 	} else {
 		return "One for you, one for me."
 
