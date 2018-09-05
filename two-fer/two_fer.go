ecc28ff470b876d8da9441a36be2d32c4d341c05
John Fink <john.fink@gmail.com>
2018-09-05 19:54:37 -0400

    There we go.

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,14 +1,15 @@
 // the twofer package contains the ShareWith function.
 package twofer
 
-// The ShareWith function takes one argument, the string variable name, and runs an if-else branch on it, returning "One for Alice,
-// one for me." if name == "Alice", "One for Bob, one for me." if name == "Bob", and "One for you, one for me." if name is anything else.
+// The ShareWith function takes one argument, the string variable name, and runs an if-else branch on it, returning "One for name, one for me."
+// if name is blank, returns "One for you, one for me."
 
 func ShareWith(name string) string {
-	if name == "Alice" || name == "Bob" {
-		return "One for " + name + ", one for me."
-	} else {
+	if name == "" {
 		return "One for you, one for me."
+	} else {
+		return "One for " + name + ", one for me."
 
 	}
+
 }
