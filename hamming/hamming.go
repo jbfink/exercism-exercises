10f1a0bf63e1e6cd9124c848eaa6561ea45cf95c
John Fink <john.fink@gmail.com>
2018-09-06 19:22:06 -0400

    Some notes.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -11,6 +11,15 @@ func Distance(a, b string) (int, error) {
 
 	if len(a1) != len(b1) {
 		return -1, nil
+	} else if a == b {
+		return 0, nil
 	}
+	// Here, a loop of some sort, nested?
+	// Like, loop elements from 0 to len(a1) (by this point a1 and b1 will have identical len)
+	// for each element:
+	//	if a1[x] == b1[x] then add 0 to distance variable
+	//      if a1[x] != b1[x] then add 1 to distance variable
+	// after len(a1), return variable, nil
+	// (why error at all? buh)
 	return 0, nil
 }
