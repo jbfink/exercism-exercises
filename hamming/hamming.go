b4c303db6b0404a7b4cec1a9fbb378b6ec70b889
John Fink <john.fink@gmail.com>
2018-09-06 19:00:41 -0400

    Split strings into slices.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,6 +1,15 @@
 package hamming
 
+//import "errors"
+import "strings"
+
+//import "fmt"
+
 func Distance(a, b string) (int, error) {
-	aarray := []byte(a)
-	barray := []byte(b)
+	a1 := strings.Split(a, "")
+	b1 := strings.Split(b, "")
+
+	if a1 == b1 {
+	}
+
 }
