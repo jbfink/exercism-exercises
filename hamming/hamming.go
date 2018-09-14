9a9d8835eb93e16bf9deae41a5bbf549a37e941d
jbfink <john.fink@gmail.com>
2018-09-13 21:21:24 -0400

    Added documentation.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,25 +3,23 @@ package hamming
 import "errors"
 import "strings"
 
-//import "fmt"
-
+// Distance function calculates the Hamming distance of two strings.
+// If the two strings are unequal in length, returns error.
+// If the two strings are identical, returns Hamming distance of zero.
+// If the two strings are of identical length, but differ in composition,
+// then a loop happens. When the two strings have different letters at the
+// same position, a counter is incremented. At the end of the loop,
+// return the contents of that counter.
 func Distance(a, b string) (int, error) {
 	a1 := strings.Split(a, "")
 	b1 := strings.Split(b, "")
 
 	if len(a1) != len(b1) {
-		return -1, errors.New("Strings are not equal.")
+		return -1, errors.New("strings are not equal")
 	} else if a == b {
 		return 0, nil
 	}
 
-	// Here, a loop of some sort, nested?
-	// Like, loop elements from 0 to len(a1) (by this point a1 and b1 will have identical len)
-	// for each element:
-	//	if a1[x] == b1[x] then add 0 to distance variable
-	//      if a1[x] != b1[x] then add 1 to distance variable
-	// after len(a1), return variable, nil
-	// (why error at all? buh)
 	distance := 0
 
 	for i := 0; i < len(a1); i++ {
@@ -33,8 +31,6 @@ func Distance(a, b string) (int, error) {
 				distance++
 			}
 		}
-
-		//		return distance, nil
 
 	}
 	return distance, nil
