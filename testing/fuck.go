1bec4f0bf8d64f1176365da0ee5a7d08f5ac2009
John Fink <john.fink@gmail.com>
2018-10-03 16:08:31 -0400

    Some futzing.

---
+++ testing/fuck.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Convert(i int) string {
+	var rain strings.Builder
+	rain.WriteString("")
+	if i%3 == 0 {
+		rain.WriteString("Pling ")
+	}
+	if i%5 == 0 {
+		rain.WriteString("Plang ")
+	}
+	if i%7 == 0 {
+		rain.WriteString("Plong")
+	}
+	if len(rain.String()) == 0 {
+		return "poop"
+	}
+	return rain.String()
+}
