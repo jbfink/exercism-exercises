7eed0905da6234f69bbc8c5fee980d0a332e3b9f
John Fink <john.fink@gmail.com>
2018-10-04 11:44:33 -0400

    Stripped out walls of text, added documentation to strip, fixed golint errors in Lookup error throw.

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,45 +1,5 @@
 package scrabble
 
-/* from README.md
-## Letter Values
-
-You'll need these:
-
-```text
-Letter                           Value
-A, E, I, O, U, L, N, R, S, T       1
-D, G                               2
-B, C, M, P                         3
-F, H, V, W, Y                      4
-K                                  5
-J, X                               8
-Q, Z                               10
-```
-
-## Examples
-
-"cabbage" should be scored as worth 14 points:
-
-- 3 points for C
-- 1 point for A, twice
-- 3 points for B, twice
-- 2 points for G
-- 1 point for E
-
-And to total:
-
-- `3 + 2*1 + 2*3 + 2 + 1`
-- = `3 + 2 + 6 + 3`
-- = `5 + 9`
-- = 14
-
-*/
-
-// adrplan:
-// first, an key-value store array thing for each letter, a=1, d=2 etc. Maybe in its own function, like func Lookup (s string) int
-// Then, split a word into letter chunks. Apply Lookup to each letter, storing the value in an int, adding the next letter value to the int.
-// Return the int.
-
 import "strings"
 import "errors"
 
@@ -47,7 +7,7 @@ import "errors"
 func Lookup(s string) (int, error) {
 	if len(s) != 1 {
 
-		return -1, errors.New("Must be one letter only.")
+		return -1, errors.New("must be one letter only")
 
 	}
 
@@ -78,6 +38,8 @@ func Lookup(s string) (int, error) {
 	}
 	return 0, nil
 }
+
+// Strip removes the error message from a given input, unless error is not nil, in which case it throws a panic.
 func Strip(i int, err error) int {
 	if err != nil {
 		panic(err)
